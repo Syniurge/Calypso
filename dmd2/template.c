Syniurge/Calypso

commit 66df399a0d0bf5f3ccd8c96db354635eef86480b

(Dangerous DMD change?) In functionResolve MATCHconvert non-template functions should not take precedence over MATCHconst template functions.
This change is essential for 999bd81cd094584a1db2357230888bf7d96e7d09. MATCHconvert was rarely returned before by TypeFunction::callMatch so setting ta_last to MATCHexact barely mattered, but now it's commonplace for C++ record types and we'd prefer avoiding an implicit ctor call if a better matching template function is available.

dmd2/template.c
@@ -2321,7 +2321,10 @@ void functionResolve(Match *m, Dsymbol *dstart, Loc loc, Scope *sc,
         LfIsBetter:
             td_best = NULL;
             ti_best = NULL;
-            ta_last = MATCHexact;
+            ta_last = (mfa >= MATCHconst) ? MATCHexact : mfa; // CALYPSO IMPORTANT WARNING not always setting ta_last to MATCHexact
+                                                        // may break code, although MATCHconvert seems to be returned by vanilla in very few cases.
+                                                        // This change is needed for @implicit ctors. If an implicit ctor is available, callMatch returns MATCHconvert
+                                                        // but we do not want a non-template function to take precedence over a better matching template function.
             m->last = mfa;
             m->lastf = fd;
             tthis_best = tthis_fd;

commit 9e4611482ab2d95285b5c22d7032ff8cb7c4f1cc

In functionResolve prefer non-template functions over template ones when MATCH values are equal and below MATCHexact.
Fixes ambiguous calls caused by 66df399a0d0bf5f3ccd8c96db354635eef86480b.

dmd2/template.c
@@ -2574,6 +2574,8 @@ void functionResolve(Match *m, Dsymbol *dstart, Loc loc, Scope *sc,
                 MATCH c1 = tf1->callMatch(tthis_fd,   fargs);
                 MATCH c2 = tf2->callMatch(tthis_best, fargs);
                 //printf("2: c1 = %d, c2 = %d\n", c1, c2);
+                if (c1 == c2 && !td_best) // CALYPSO renew preference for non-template functions over template functions here
+                    c2 = MATCHexact;
                 if (c1 > c2) goto Ltd;
                 if (c1 < c2) goto Ltd_best;
             }