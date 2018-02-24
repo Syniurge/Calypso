bcee54e59e06276dbf3d3f01264b3bff4b5cbf78
Elie Morisse <syniurge@gmail.com>
2018-02-24 09:50:30 -0300

    Differentiate C++ catch clauses from D ones by wrapping std::type_info pointers inside __cpp_type_info_ptr class instances like DMD does. (#2590)
    Previously assuming in the personality routine that encountering a C++ exception meant that catch clauses were raw std::type_info pointers was wrong in cases like:
    
    try {
       throwStdException(); // external C++ function throwing a std::exception
    } catch (Throwable t) {
       (...)
    }
    
    Generated __cpp_type_info_ptr constants receive the same mangling that they do within DMD.
    
    (cherry picked from commit bf8cd786e7f66cd02787c887f28d03391ef0a22b)

--- gen/trycatchfinally.cpp
+++ gen/trycatchfinally.cpp
@@ -16,10 +16,13 @@
 #include "gen/classes.h"
 #include "gen/funcgenstate.h"
 #include "gen/llvmhelpers.h"
+#include "gen/mangling.h"
 #include "gen/ms-cxx-helper.h"
+#include "gen/rttibuilder.h"
 #include "gen/runtime.h"
 #include "gen/tollvm.h"
 #include "ir/irfunction.h"
+#include "ir/irtypeclass.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -177,9 +180,28 @@ void TryCatchScope::emitCatchBodies(IRState &irs, llvm::Value *ehPtrSlot) {
       DtoResolveClass(cd);
       if (cd->isCPPclass()) {
         const char *name = Target::cppTypeInfoMangle(cd);
-        ci = getOrCreateGlobal(
+        auto cpp_ti = getOrCreateGlobal(
             cd->loc, irs.module, getVoidPtrType(), /*isConstant=*/true,
             LLGlobalValue::ExternalLinkage, /*init=*/nullptr, name);
+
+        // Wrap std::type_info pointers inside a __cpp_type_info_ptr class instance so that
+        // the personality routine may differentiate C++ catch clauses from D ones.
+        OutBuffer mangleBuf;
+        mangleBuf.writestring("_D");
+        mangleToBuffer(cd, &mangleBuf);
+        mangleBuf.printf("%d%s", 18, "_cpp_type_info_ptr");
+        const auto wrapperMangle = getIRMangledVarName(mangleBuf.peekString(), LINKd);
+
+        RTTIBuilder b(ClassDeclaration::cpp_type_info_ptr);
+        b.push(cpp_ti);
+
+        auto wrapperType = llvm::cast<llvm::StructType>(
+            static_cast<IrTypeClass*>(ClassDeclaration::cpp_type_info_ptr->type->ctype)->getMemoryLLType());
+        auto wrapperInit = b.get_constant(wrapperType);
+
+        ci = getOrCreateGlobal(
+            cd->loc, irs.module, wrapperType, /*isConstant=*/true,
+            LLGlobalValue::LinkOnceODRLinkage, wrapperInit, wrapperMangle);
       } else {
         ci = getIrAggr(cd)->getClassInfoSymbol();
       }
